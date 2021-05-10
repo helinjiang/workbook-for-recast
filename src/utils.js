2436452dcd2e3d53d6ae7fd46726d1479b1a5747
helinjiang <onehlj@qq.com>
2021-05-10 14:57:18 +0800

    feat: add demo 100

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,16 @@
+const fs = require('fs');
+
 function printByConsole(content, opts = {}) {
   console.log(`\n=========== ${opts.tag} begin ===========`);
   console.log(content);
   console.log(`=========== ${opts.tag} end ===========\n`);
 }
 
+function readFileSync(file) {
+  return fs.readFileSync(file, { encoding: 'utf8' });
+}
+
 module.exports = {
   printByConsole,
+  readFileSync,
 };
