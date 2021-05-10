c1ba4f0cae4d73a2f29f27e6247776fe830f9c6e
helinjiang <onehlj@qq.com>
2021-05-10 11:17:49 +0800

    feat: 创建变量申明表达式

--- src/001-variableDeclaration/demo03.js
+++ src/001-variableDeclaration/demo03.js
@@ -25,9 +25,24 @@ ast.program.body[4] = b.variableDeclaration('const', [
   b.variableDeclarator(b.identifier('e2'), b.literal('42')),
 ]);
 
-// var a = 1;
-// let b = 2;
-// const c = "3";
-// let d;
-// const e1 = "41", e2 = "42";
+// const f = {
+//   fkey1: "f1",
+//   fkey2: 2
+// };
+ast.program.body[5] = b.variableDeclaration('const', [
+  b.variableDeclarator(b.identifier('f'), b.objectExpression([
+    b.objectProperty(b.identifier('fkey1'), b.literal('f1')),
+    b.objectProperty(b.identifier('fkey2'), b.literal(2)),
+  ])),
+]);
+
+// const g = [g1, 2, "3"];
+ast.program.body[6] = b.variableDeclaration('const', [
+  b.variableDeclarator(b.identifier('g'), b.arrayExpression([
+    b.identifier('g1'),
+    b.literal(2),
+    b.literal('3'),
+  ])),
+]);
+
 printByConsole(recast.print(ast).code, { tag: '修改之后' });
