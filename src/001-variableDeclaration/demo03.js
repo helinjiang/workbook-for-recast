8303439bca5f66b0aa15949af00a0f33425aedc3
helinjiang <onehlj@qq.com>
2021-05-10 10:26:49 +0800

    feat: 凭空创建两个变量定义

--- src/001-variableDeclaration/demo03.js
+++ src/001-variableDeclaration/demo03.js
@@ -7,11 +7,20 @@ printByConsole(recast.print(ast).code, { tag: '修改之前' });
 
 const b = recast.types.builders;
 
+// var a = 1;
 ast.program.body[0] = b.variableDeclaration('var', [b.variableDeclarator(b.identifier('a'), b.literal(1))]);
+
+// let b = 2;
 ast.program.body[1] = b.variableDeclaration('let', [b.variableDeclarator(b.identifier('b'), b.literal(2))]);
+
+// let b = 2;
 ast.program.body[2] = b.variableDeclaration('const', [b.variableDeclarator(b.identifier('c'), b.literal('3'))]);
+
+// let d;
+ast.program.body[3] = b.variableDeclaration('let', [b.variableDeclarator(b.identifier('d'), null)]);
 
 // var a = 1;
 // let b = 2;
 // const c = "3";
+// let d;
 printByConsole(recast.print(ast).code, { tag: '修改之后' });
